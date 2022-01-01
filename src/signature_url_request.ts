25cd04ade95f334c69e4519e9412e301cec34d1f
Shota Kubota <ta2@o2p.jp>
2022-01-01 22:38:40 +0900

    feat(src): implemented signature URL request function

---
+++ src/signature_url_request.ts
@@ -0,0 +1,30 @@
+import { S3RequestPresigner } from "@aws-sdk/s3-request-presigner";
+import { Hash } from "@aws-sdk/hash-node";
+import { HttpRequest } from "@aws-sdk/protocol-http";
+import { parseUrl } from "@aws-sdk/url-parser";
+
+type Params = {
+  url: string;
+};
+
+export async function requestSignatureUrl({ url }: Params): Promise<string> {
+  const hash = Hash.bind(null, "sha256");
+
+  const presigner = new S3RequestPresigner({
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+    region: process.env.AWS_S3_REGION_NAME,
+    sha256: hash,
+  });
+
+  const { hostname, path, protocol, query } = await presigner.presign(
+    new HttpRequest(parseUrl(url)),
+    { expiresIn: 600 }
+  );
+  const urlSearchParams = new URLSearchParams(query as Record<string, string>);
+  const urlSearchParamString = urlSearchParams.toString();
+
+  return `${protocol}//${hostname}${path}?${urlSearchParamString}`;
+}
