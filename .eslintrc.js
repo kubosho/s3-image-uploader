3aff5ea2948cabaf4cd5586e3b28ef805808169a
Shota Kubota <ta2@o2p.jp>
2022-01-02 22:16:11 +0900

    test(eslint): add ESLint config

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: { browser: true, node: true, es6: true, jest: true },
+  extends: [
+    'eslint:recommended',
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  rules: {
+    '@next/next/no-img-element': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-use-before-define': ['error', { functions: false, classes: true, variables: true }],
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
+      {
+        allowExpressions: true,
+        allowTypedFunctionExpressions: true,
+        allowHigherOrderFunctions: true,
+      },
+    ],
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+};
