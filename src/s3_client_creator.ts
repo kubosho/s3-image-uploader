b88f19c7b6b53a9997ad02baf246c6ade5f42eb5
Shota Kubota <ta2@o2p.jp>
2022-01-08 23:23:48 +0900

    chore(s3_client_creator): add return type

--- src/s3_client_creator.ts
+++ src/s3_client_creator.ts
@@ -1,6 +1,6 @@
 import { S3Client } from '@aws-sdk/client-s3';
 
-export const createS3Client = () =>
+export const createS3Client = (): S3Client =>
   new S3Client({
     region: process.env.AWS_S3_REGION_NAME,
   });
