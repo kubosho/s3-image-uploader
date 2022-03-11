be10b3e090ec9cba2f5faecf99897deb9ac05961
Shota Kubota <ta2@o2p.jp>
2022-03-11 20:56:30 +0900

    feat(ImageDetailModal): improve ImageDetailModal design

--- src/components/ImageDetailModal/ImageDetailModal.tsx
+++ src/components/ImageDetailModal/ImageDetailModal.tsx
@@ -18,20 +18,34 @@ export function ImageDetailModal({ name, url, alt, open }: Props): JSX.Element {
 
   return createPortal(
     <dialog
-      className={classNames('bg-slate-900 bg-opacity-80 fixed left-0 top-0 w-full h-full', {
+      className={classNames(
+        'bg-slate-900 bg-opacity-80 fixed flex flex-col items-center justify-center left-0 top-0 w-full h-full',
+        {
         hidden: url === '',
-      })}
+        },
+      )}
       open={open}
     >
+      <div className="flex flex-col">
       <img src={url} alt="" width={800} />
       <p>{name}</p>
-      <div>
-        <label htmlFor="alt-text">画像の代替テキスト</label>
-        <textarea id="alt-text" name="alt-text" defaultValue={alt} />
+        <div className="w-full">
+          <label className="text-white" htmlFor="alt-text">
+            画像の代替テキスト
+          </label>
+          <textarea className="w-full" id="alt-text" name="alt-text" defaultValue={alt} />
+          <label className="text-white" htmlFor="embedded-code">
+            貼り付け用コード
+          </label>
+          <input
+            className="w-full"
+            type="text"
+            value={`![${alt}](${modifiedUrl.current})`}
+            id="embedded-code"
+            name="embedded-code"
+            readOnly
+          />
       </div>
-      <div>
-        <label htmlFor="embedded-code">貼り付け用コード</label>
-        <input type="text" value={`![${alt}](${url})`} id="embedded-code" name="embedded-code" readOnly />
       </div>
     </dialog>,
     document.body,
