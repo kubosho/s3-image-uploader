1ae6ad056829c85783d475b58c91a33f57486138
Shota Kubota <ta2@o2p.jp>
2022-03-11 17:21:12 +0900

    refactor(pages/index): remove unnecessary codes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
 import { useCallback, useEffect, useState } from 'react';
-import { Subscription } from 'rxjs';
-import { useInstance } from '../hooks/use_instance';
 import { ImageDetailModal } from '../components/ImageDetailModal';
 import { keyboardEventOnIndexPageObservable } from '../shared_logic/keyboard_shortcut/keyboard_event_observable';
 import { IndexPageShortcutKey } from '../shared_logic/keyboard_shortcut/shortcut_keys';
@@ -15,22 +13,19 @@ type Props = {
 function Index({ imageUrls }): JSX.Element {
   const [imageUrl, setImageUrl] = useState('');
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const keyboardEvent$ = useInstance(() => keyboardEventOnIndexPageObservable());
-  const subscription = useInstance(() => new Subscription());
 
   useEffect(() => {
-    subscription.add(
-      keyboardEvent$.subscribe((key) => {
-        if (isModalOpen && key === IndexPageShortcutKey.ModalExit) {
-          setIsModalOpen(false);
-        }
-      }),
-    );
+    const obserevable = keyboardEventOnIndexPageObservable();
+    const subscription = obserevable.subscribe((key) => {
+      if (isModalOpen && key === IndexPageShortcutKey.ModalExit) {
+        setIsModalOpen(false);
+      }
+    });
 
     return () => {
       subscription.unsubscribe();
     };
-  }, [isModalOpen, keyboardEvent$, subscription]);
+  }, [isModalOpen]);
 
   const onClick = useCallback((url: string) => {
     setImageUrl(url);
