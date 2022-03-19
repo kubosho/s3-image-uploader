86b658fd5cff0d550606e66722958ddca86791fa
Shota Kubota <ta2@o2p.jp>
2022-03-19 17:44:27 +0900

    feat(tailwind): add navy color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        navy: {
+          100: '#0086ed',
+          200: '#0076d1',
+          300: '#0067b5',
+          400: '#005799',
+          500: '#00477D',
+          600: '#003760',
+          700: '#002745',
+          800: '#001729',
+        },
+      },
+    },
   },
   plugins: [],
 };
