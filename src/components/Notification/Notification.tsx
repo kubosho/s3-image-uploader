393d6c1255644d3aeeecc315c160573869569642
Shota Kubota <ta2@o2p.jp>
2022-03-19 18:22:57 +0900

    feat(Notification): improve Notification component
    - added automatic focus to button element
    - added live region
    - tweak styles

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from 'react';
+
 type Props = {
   isShown: boolean;
   text: string;
@@ -5,14 +7,28 @@ type Props = {
   onClickCloseButton: () => void;
 };
 
-export const Notification = ({ isShown, text, buttonText, onClickCloseButton }: Props): JSX.Element =>
-  isShown && (
-    <div role="alertdialog" aria-describedby="notificationDescription">
-      <div className="flex" role="document" tabIndex={0}>
-        <p id="notificationDescription">{text}</p>
-        <button type="button" onClick={onClickCloseButton}>
-          {buttonText}
-        </button>
+export const Notification = ({ isShown, text, buttonText, onClickCloseButton }: Props): JSX.Element => {
+  const ref = useRef<HTMLButtonElement | null>(null);
+
+  useEffect(() => {
+    ref.current && ref.current.focus();
+  }, [isShown]);
+
+  return (
+    isShown && (
+      <div
+        className="p-3 border border-navy-600 bg-slate-100 text-slate-900"
+        role="alertdialog"
+        aria-describedby="notificationDescription"
+        aria-live="polite"
+      >
+        <div className="flex" role="document">
+          <p id="notificationDescription">{text}</p>
+          <button ref={ref} type="button" onClick={onClickCloseButton}>
+            {buttonText}
+          </button>
+        </div>
       </div>
-    </div>
+    )
   );
+};
