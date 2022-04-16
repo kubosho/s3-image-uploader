75dd696ea2a2e9528e7ac825ce9558edc08c85bd
Shota Kubota <ta2@o2p.jp>
2022-04-16 22:26:44 +0900

    build(nextjs): add env configs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  env: {
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    AWS_S3_BUCKET_NAME: process.env.AWS_S3_BUCKET_NAME,
+    AWS_S3_HOST_NAME: process.env.AWS_S3_HOST_NAME,
+    AWS_S3_REGION_NAME: process.env.AWS_S3_REGION_NAME,
+  },
   images: {
     domains: [`${process.env.AWS_S3_BUCKET_NAME}.${process.env.AWS_S3_HOST_NAME}`],
   },
