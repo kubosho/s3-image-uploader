7862ad6bee44ef0b2b32021f26958c6eb567ba54
Shota Kubota <ta2@o2p.jp>
2022-04-30 19:46:34 +0900

    feat(pages/index): add rendering a dedicated component in case of an error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { ChangeEvent, useCallback, useEffect, useState } from 'react';
+import { ErrorReason, ERROR_REASON } from '../constants/error_reason';
 import { ImageDetail } from '../components/ImageDetail';
 import { keyboardEventOnIndexPageObservable } from '../shared_logic/keyboard_shortcut/keyboard_event_observable';
 import { IndexPageShortcutKey } from '../shared_logic/keyboard_shortcut/shortcut_keys';
@@ -7,19 +8,23 @@ import { createS3Client } from '../shared_logic/s3/s3_client_creator';
 import { fetchImageUrlList } from '../shared_logic/s3/image_url_fetcher';
 import { Notification } from '../components/Notification';
 import { SiteHeader } from '../components/SiteHeader';
-import { putObject } from '../shared_logic/s3/object_put';
 import { convertImageFileToUint8Array } from '../shared_logic/convert_image_file_to_uint8array';
+import { hasAwsEnv } from '../shared_logic/has_aws_env';
+import { putObject } from '../shared_logic/s3/object_put';
 import { UploadButton } from '../components/UploadButton';
 import { ImageList } from '../components/ImageList';
+import { Error } from '../components/Error';
 
 type Props = {
   imageUrls: string[];
+  isError?: boolean;
+  errorReason?: ErrorReason;
 };
 
 const SECONDS_TO_EXPIRE = 600;
 const S3_CLIENT = createS3Client();
 
-function Index({ imageUrls: initialImageUrls }): JSX.Element {
+function Index({ imageUrls: initialImageUrls, isError, errorReason }): JSX.Element {
   const [imageUrls, setImageUrls] = useState(initialImageUrls);
   const [imageUrl, setImageUrl] = useState('');
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -93,6 +98,10 @@ function Index({ imageUrls: initialImageUrls }): JSX.Element {
     };
   }, [isModalOpen]);
 
+  if (isError) {
+    return <Error errorReason={errorReason} />;
+  }
+
   return (
     <>
       <SiteHeader siteTitle="S3 image uploader" />
@@ -114,6 +123,16 @@ function Index({ imageUrls: initialImageUrls }): JSX.Element {
 }
 
 export async function getStaticProps(): Promise<{ props: Props }> {
+  if (!hasAwsEnv(process.env)) {
+    return {
+      props: {
+        imageUrls: [],
+        isError: true,
+        errorReason: ERROR_REASON.NOT_SET_AWS_ENVIRONMENT_VARIABLES,
+      },
+    };
+  }
+
   const imageUrls = await fetchImageUrlList(S3_CLIENT, SECONDS_TO_EXPIRE);
 
   return {
