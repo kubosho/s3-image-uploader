c90450506d1ab1e141a3a68037060e4206931500
Shota Kubota <ta2@o2p.jp>
2022-05-04 00:57:45 +0900

    fixup! feat(Image): implement alt text edit

--- src/components/AltTextEdit/AltTextEdit.tsx
+++ src/components/AltTextEdit/AltTextEdit.tsx
@@ -6,9 +6,7 @@ type Props = {
 export function AltTextEdit({ initialText, onClickSubmit }: Props): JSX.Element {
   return (
     <>
-      <label className="text-white" htmlFor="alt-text">
-        Alt
-      </label>
+      <label htmlFor="alt-text">Alt</label>
       <textarea className="w-full" id="alt-text" name="alt-text" defaultValue={initialText} />
       <button type="button" onClick={onClickSubmit}>
         Submit
