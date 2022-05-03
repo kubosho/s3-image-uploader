f82b06a517c20aaf6e053175ee2629184287bd6d
Shota Kubota <ta2@o2p.jp>
2022-05-04 00:26:36 +0900

    feat(components): implement AltTextEdit component

---
+++ src/components/AltTextEdit/AltTextEdit.tsx
@@ -0,0 +1,18 @@
+type Props = {
+  initialText?: string;
+  onClickSubmit?: () => void;
+};
+
+export function AltTextEdit({ initialText, onClickSubmit }: Props): JSX.Element {
+  return (
+    <>
+      <label className="text-white" htmlFor="alt-text">
+        Alt
+      </label>
+      <textarea className="w-full" id="alt-text" name="alt-text" defaultValue={initialText} />
+      <button type="button" onClick={onClickSubmit}>
+        Submit
+      </button>
+    </>
+  );
+}
---
+++ src/components/AltTextEdit/__stories__/AltTextEdit.stories.tsx
@@ -0,0 +1,24 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { AltTextEdit } from '../AltTextEdit';
+
+export default {
+  title: 'components/AltTextEdit',
+  component: AltTextEdit,
+} as ComponentMeta<typeof AltTextEdit>;
+
+function noop(): void {
+  return;
+}
+
+const Template: ComponentStory<typeof AltTextEdit> = (args) => <AltTextEdit {...args} />;
+
+export const AltTextEditComponent = Template.bind({});
+AltTextEditComponent.args = {
+  onClickSubmit: noop,
+};
+
+export const AltTextEditWithInitialText = Template.bind({});
+AltTextEditWithInitialText.args = {
+  initialText: 'Test initial text',
+  onClickSubmit: noop,
+};
