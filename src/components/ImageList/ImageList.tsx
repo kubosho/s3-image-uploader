7d19a4f2c97e9659fab363ceb6a4bfc4458bcd7e
Shota Kubota <ta2@o2p.jp>
2022-05-04 16:05:26 +0900

    feat(ImageList): use Image component

--- src/components/ImageList/ImageList.tsx
+++ src/components/ImageList/ImageList.tsx
@@ -1,4 +1,4 @@
-import { ImageEdit } from '../ImageEdit';
+import { Image } from '../Image';
 
 type Props = {
   imageUrls: string[];
@@ -19,13 +19,7 @@ export function ImageList({ imageUrls }: Props): JSX.Element {
     <ul className="box-border columns-5 mt-4">
       {imageData.map(({ name, url }, index) => (
         <li key={index} className="break-inside-avoid grid grid-flow-row">
-          <div className="relative">
-            <p className="row-start-2 break-all">{name}</p>
-            <img className="row-start-1" src={url} alt="" width="auto" height="300" />
-            <div className="row-start-3 absolute top-0 right-0 z-10">
-              <ImageEdit url={url} alt="" />
-            </div>
-          </div>
+          <Image imageName={name} imageUrl={url} />
         </li>
       ))}
     </ul>
