8f9f12aba8201d1069f417528f93cdf8375d079e
KUBOTA Shota <ta2+github@o2p.jp>
2025-11-03 21:02:47 +0900

    chore: remove unused Storybook plugins

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,8 +2,6 @@ module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
     {
       name: '@storybook/addon-postcss',
       options: {
