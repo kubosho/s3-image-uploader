f995b95b222a24b27eb2169dee9d0c8d1c3822dd
KUBOTA Shota <ta2+github@o2p.jp>
2025-11-09 18:40:50 +0900

    feat(tailwind): use O2 Project design system

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,19 +1,10 @@
+import designSystem from "@o2project/design-system/tailwind.config.js";
+
 const config = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      colors: {
-        navy: {
-          100: '#0086ed',
-          200: '#0076d1',
-          300: '#0067b5',
-          400: '#005799',
-          500: '#00477D',
-          600: '#003760',
-          700: '#002745',
-          800: '#001729',
-        },
-      },
+      colors: designSystem.colors,
     },
   },
   plugins: [],
