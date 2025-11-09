73ea275e8c1d4c8dde26a23ef74cc85264c9c97d
KUBOTA Shota <ta2+github@o2p.jp>
2025-11-09 18:21:51 +0900

    chore: migrate ESLint config to flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import config from "@kubosho/configs/eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+import storybook from "eslint-plugin-storybook";
+
+export default defineConfig([
+  ...config,
+  globalIgnores([
+    ".next/**",
+    "build/**",
+    "next-env.d.ts",
+    "out/**",
+  ]),
+  {
+    files: ["**/*.stories.ts", "**/*.stories.tsx"],
+    extends: [storybook.configs["flat/recommended"]],
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      "import/no-extraneous-dependencies": [
+        "error",
+        {
+          devDependencies: [
+            "**/.storybook/**/*.ts",
+            "**/*.config.*",
+            "**/*.stories.*",
+            "**/*.test.*",
+          ],
+        },
+      ],
+    },
+  },
+]);
