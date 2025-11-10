cde391b31974925b6e70a7dfa66c033051576d55
KUBOTA Shota <ta2+github@o2p.jp>
2025-11-10 11:07:51 +0900

    feat(components): add auth buttons in SiteHeader

--- src/components/SiteHeader/SiteHeader.tsx
+++ src/components/SiteHeader/SiteHeader.tsx
@@ -1,15 +1,20 @@
 import Link from 'next/link';
 
-type Props = {
-  siteTitle: string;
-};
+import { auth } from '../../features/auth/auth';
+import { SignInButton } from '../../features/auth/components/SignInButton';
+import { SignOutButton } from '../../features/auth/components/SignOutButton';
 
-export const SiteHeader = ({ siteTitle }: Props): React.JSX.Element => (
-  <header className="p-3 bg-navy-600">
-    <h1>
-      <Link href="/" className="text-white">
-        {siteTitle}
-      </Link>
-    </h1>
-  </header>
-);
+export const SiteHeader = async (): Promise<React.JSX.Element> => {
+  const session = await auth();
+
+  return (
+    <header className="bg-primary-main flex items-center justify-between p-4">
+      <h1>
+        <Link href="/" className="text-white">
+          S3 image uploader
+        </Link>
+      </h1>
+      {session?.user != null ? <SignOutButton /> : <SignInButton />}
+    </header>
+  );
+};
